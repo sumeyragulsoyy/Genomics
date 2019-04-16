21b32c19973d5f699f31691f2f7648cd9dc12cdd
sumeyragulsoyy <32885719+sumeyragulsoyy@users.noreply.github.com>
2019-04-16 08:25:19 +0300

    Prints are fixed and some constraints added

--- assignment1/genom.py
+++ assignment1/genom.py
@@ -1,19 +1,28 @@
 import re 
 k=input('Enter k numbers: ')
-x=input('Enter at least number: ')
 file=input('Enter file name : ')
 mylist=[]
 rev={'A':'T','T':'A','G':'C','C':'G'}
 mydict={}
 possibleKmer=[]
-with open(file,'r') as f:
-        for line in f:
-            for i in range(len(line)-int(k)):
-                pattern=line[i:i+int(k)] #get pattern
-                p = re.compile(pattern) 
-                kmer=p.findall(line) #find all existence in line ,return list
-                mylist.append(kmer) #add to list each kmer 
+flagg=0
+while flagg==0:
+    if int(k)<=9:
+        with open(file,'r') as f:
+                for line in f:
+                    for i in range(len(line)-int(k)):
+                        pattern=line[i:i+int(k)] #get pattern
+                        p = re.compile(pattern) 
+                        kmer=p.findall(line) #find all existence in line ,return list
+                        mylist.append(kmer) #add to list each kmer 
+        flagg=1         
+    else:
+        print("k can be at most 9")
+        k=input('Enter k numbers: ')
 
+x=input('Enter at least number: ')
+
+        
 for g in range(len(mylist)):
     mydict[mylist[g][0]]=len(mylist[g]) #'ACA' -> 3 ,kmer and count in line as a dictionary
 #Print all possible kmer that satisfied the given condition ,at least x times
@@ -21,7 +30,7 @@ flag1=0 #to print kmer that satisfied the given condition
 flag2=0 #to print reverse complement of possible k mers
 for keys in mydict: #keys -> string
     z='' # revers of each k mer
-    if mydict.get(keys) >= int(x):
+    if int(x)>1 and mydict.get(keys) >= int(x) :
         flag1=1
         print(keys +' : ' +str(mydict.get(keys)))
         #get reverse comlement of possible k-mer
